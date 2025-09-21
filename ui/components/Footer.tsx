de4fd7af2c12c23f4b0a46ea45a403d61798cae3
Berima1 <Eddy199099@gmail.com>
2025-09-21 07:09:46 +0000

    Create Footer.tsx

---
+++ ui/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white mt-12">
+      <div className="container mx-auto p-6 text-center">
+        <p>&copy; 2025 Anaasɛ Portal. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
